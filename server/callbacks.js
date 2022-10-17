0599bc4ae3e418faef4b33b8c9260d60c089b8cc
Narwhalish <bryao@seas.upenn.edu>
2022-10-17 12:55:02 -0400

    Use callbacks to calculate player score and use constants.js file to dynamically create tasks.

--- server/callbacks.js
+++ server/callbacks.js
@@ -3,7 +3,7 @@ import Empirica from "meteor/empirica:core";
 // onGameStart is triggered opnce per game before the game starts, and before
 // the first onRoundStart. It receives the game and list of all the players in
 // the game.
-Empirica.onGameStart(game => {});
+Empirica.onGameStart((game) => {});
 
 // onRoundStart is triggered before each round starts, and before onStageStart.
 // It receives the same options as onGameStart, and the round that is starting.
@@ -20,16 +20,20 @@ Empirica.onStageEnd((game, round, stage) => {});
 // onRoundEnd is triggered after each round.
 // It receives the same options as onGameEnd, and the round that just ended.
 Empirica.onRoundEnd((game, round) => {
-  game.players.forEach(player => {
+  game.players.forEach((player) => {
     const value = player.round.get("value") || 0;
     const prevScore = player.get("score") || 0;
-    player.set("score", prevScore + value);
+    let newScore = 1 - value / round.get("correctAnswer");
+
+    newScore = newScore < 0 ? 0 : newScore;
+
+    player.set("score", prevScore + newScore);
   });
 });
 
 // onGameEnd is triggered when the game ends.
 // It receives the same options as onGameStart.
-Empirica.onGameEnd(game => {});
+Empirica.onGameEnd((game) => {});
 
 // ===========================================================================
 // => onSet, onAppend and onChange ==========================================
--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import Empirica from "meteor/empirica:core";
 import "./bots.js";
 import "./callbacks.js";
 
+import { taskData } from "./constants";
+
 // gameInit is where the structure of a game is defined.
 // Just before every game starts, once all the players needed are ready, this
 // function is called with the treatment and the list of players.
@@ -9,18 +11,34 @@ import "./callbacks.js";
 // and the players. You can also get/set initial values on your game, players,
 // rounds and stages (with get/set methods), that will be able to use later in
 // the game.
-Empirica.gameInit(game => {
+Empirica.gameInit((game) => {
+  const nodes = _.range(game.players.length);
+
   game.players.forEach((player, i) => {
     player.set("avatar", `/avatars/jdenticon/${player._id}`);
     player.set("score", 0);
+
+    player.set("nodeId", i);
+
+    const networkNeighbors = nodes.filter((node) => node.i !== i);
+    player.set("neighbors", networkNeighbors);
   });
 
-  _.times(10, i => {
-    const round = game.addRound();
+  Object.keys(taskData).forEach((taskName) => {
+    const task = taskData[taskName];
+    const round = game.addRound({
+      data: {
+        taskName: taskName,
+        questionText: task.questionText,
+        imagePath: task.path,
+        correctAnswer: task.correctAnswer,
+      },
+    });
+
     round.addStage({
       name: "response",
       displayName: "Response",
-      durationInSeconds: 120
+      durationInSeconds: 120,
     });
   });
 });
